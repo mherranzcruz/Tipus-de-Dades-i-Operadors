38a61a3795bb211b4f2dda093ca13a9f859d1f08
mherranzcruz2017 <36241957+mherranzcruz@users.noreply.github.com>
2018-04-09 20:22:13 +0200

    Exercici 11

---
+++ Exercici_11_tipus__de_dades_i_operadors/Exercici_11_tipus__de_dades_i_operadors.ino
@@ -0,0 +1,40 @@
+/*****************************************************************
+**                                                              **
+**             Títol:  Tipus de dades i operadors               **
+**          Proba error en resultat divisió amb int             **
+**                                                              **
+** NOM: Martin Herranz Cruz                    Data:22/01/2018  **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+float a = 3;
+float b = 2;
+float d;
+//*****************************SETUP******************************
+
+void setup()               // run once, when the sketch starts
+{
+  Serial.begin(9600);      // set up Serial library at 9600 bps
+
+  Serial.println("Here is division: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+
+  d = a / b;
+ 
+  Serial.print("a / b = ");
+  Serial.println(d);
+}
+//******************************LOOP******************************
+
+void loop() {                 //Inicia el bucle del programa
+
+
+}
+
+//****************************FUNCIONS****************************
